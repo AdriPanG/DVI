f7b665026749f5f5e12923f27a1e41c97b8adf3c
AdriPanG <adripang92@gmail.com>
2017-02-22 15:18:08 +0100

    Practica 1 de DVI (MemoryGame)

---
+++ practica1/src/practica1.js
@@ -0,0 +1,27 @@
+/**
+ * MemoryGame es la clase que representa nuestro juego. Contiene un array con la cartas del juego,
+ * el número de cartas encontradas (para saber cuándo hemos terminado el juego) y un texto con el mensaje
+ * que indica en qué estado se encuentra el juego
+ */
+var MemoryGame = MemoryGame || {};
+
+/**
+ * Constructora de MemoryGame
+ */
+MemoryGame = function(gs) {
+
+};
+
+
+
+/**
+ * Constructora de las cartas del juego. Recibe como parámetro el nombre del sprite que representa la carta.
+ * Dos cartas serán iguales si tienen el mismo sprite.
+ * La carta puede guardar la posición que ocupa dentro del tablero para luego poder dibujarse
+ * @param {string} id Nombre del sprite que representa la carta
+ */
+MemoryGame.Card = function(id) {
+
+};
+
+
