057d7c8f76b8160204d3b8d9665887a539ba9159
WyrnCael <juanjpri@ucm.es>
2017-02-25 16:13:47 +0100

    Añadidos metodos initGame y draw en MemoryGame

--- practica1/src/practica1.js
+++ practica1/src/practica1.js
@@ -9,9 +9,28 @@ var MemoryGame = MemoryGame || {};
  * Constructora de MemoryGame
  */
 MemoryGame = function(gs) {
+	var cartas = ["8-ball", "potato", "dinosaur", "kronos", "rocket", "unicorn", "guy", "zeppelin"];
+	var nCartasEncontradas = 0;
+	var textoEstado = "Memory Game";
+	var tablero = [];
 
-};
+	this.initGame = function(){		
+		// Aqui hay que desordenar las cartas, de momento las insertamos dos veces en orden
+		tablero = ["8-ball", "potato", "dinosaur", "kronos", "rocket", "unicorn", "guy", "zeppelin", 
+					"8-ball", "potato", "dinosaur", "kronos", "rocket", "unicorn", "guy", "zeppelin"]
+	}
 
+	this.draw = function(){
+		// Mostramos el estado actual
+		gs.drawMessage(textoEstado);
+
+		// Mostramos las cartas
+		for(var i = 0; i < tablero.length ; i++){
+			gs.draw(tablero[i], i);
+		}
+	}	
+
+}
 
 
 /**
