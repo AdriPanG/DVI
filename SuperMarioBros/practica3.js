75e30ecf871cef2d885b20aef03e2a6a436f635a
AdriPanG <adripang92@gmail.com>
2017-04-11 16:10:43 +0100

    Añadido Goomba y que se reinicie el nivel cuando Mario se cae o muere

--- SuperMarioBros/practica3.js
+++ SuperMarioBros/practica3.js
@@ -13,7 +13,7 @@ window.addEventListener("load",function() {
         		this._super(p, {
         			sheet: "marioR",
         			sprite: "mario_small",
-        			jumpSpeed: -400,
+        			jumpSpeed: -420,
         			speed: 200,
         			x: 150,
         			y: 380,
@@ -24,24 +24,67 @@ window.addEventListener("load",function() {
         	},
 
         	step: function(dt) {
-
+        		if(this.p.y > 580){
+					//Q.stageScene("endGame", 1, {label: "Game over"});
+					this.p.x = 150;
+					this.p.y = 380;
+				}
         	}
 
         });
 
+        Q.Sprite.extend("Goomba",{
+        	
+        	init: function(p) {
+        		this._super(p, {
+        			sheet: "goomba",
+        			sprite: "goomba",
+        			speed: 180,
+        			frame: 0,
+        			vx: 100,
+        			x: 800,
+        			y: 380,
+        			direction: "left",
+        		});
+
+        		this.add('2d, aiBounce');
+
+        		this.on("bump.left,bump.right,bump.bottom",function(collision){
+        			if(collision.obj.isA("Mario")) {
+        				//Q.stageScene("endGame", 1, {label: "You Died"});
+        				collision.obj.destroy();
+        				Q.stageScene("level1");
+        			}
+        		});
+
+        		this.on("bump.top",function(collision){
+        			if(collision.obj.isA("Mario")) {
+        				this.destroy();
+        				collision.obj.p.vy = -200;
+        			}
+        		});
+        	},
+
+        	step: function(dt) {
+
+        	}
+        });
 
         Q.scene("level1",function(stage) {
             Q.stageTMX("level.tmx",stage);
 
             var player = stage.insert(new Q.Mario());
 
+            stage.insert(new Q.Goomba());
+
             stage.add("viewport").follow(player);
             stage.viewport.offsetX = -20;
-			stage.viewport.offsetY = 150;
+			stage.viewport.offsetY = 160;
         });   
 
-        Q.loadTMX("level.tmx, mario_small.png, mario_small.json", function() {
-        	Q.compileSheets("mario_small.png","mario_small.json");
+        Q.loadTMX("level.tmx, mario_small.png, mario_small.json, goomba.png, goomba.json", function() {
+        	Q.compileSheets("mario_small.png", "mario_small.json");
+        	Q.compileSheets("goomba.png", "goomba.json");
             Q.stageScene("level1");
         });   
 
