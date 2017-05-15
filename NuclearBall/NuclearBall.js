1c70da3f0300b12875be6a45c3f14745648dbee6
WyrnCael <juanjpri@ucm.es>
2017-05-15 20:32:55 +0200

    Iniciando codigo y conversión a tileSet

---
+++ NuclearBall/NuclearBall.js
@@ -0,0 +1,18 @@
+window.addEventListener("load",function() {
+
+	var Q = Quintus({audioSupported: [ 'mp3','ogg' ]})
+        .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI, TMX, Audio")
+        .setup({
+            width: 760,
+            height: 480,
+    }).controls().touch().enableSound();
+
+   	Q.scene("mainTitle",function(stage) {
+		Q.stageTMX("level1.tmx",stage);
+	});
+
+    Q.loadTMX("level1.tmx", function() {
+    	Q.stageScene("mainTitle");
+    });   
+
+});
