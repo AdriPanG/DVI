890cae5af1cafd4617ce71b1c7010e73450b760c
WyrnCael <juanjpri@ucm.es>
2017-05-17 14:27:57 +0200

    Reescalado del mapa

--- NuclearBall/NuclearBall.js
+++ NuclearBall/NuclearBall.js
@@ -3,12 +3,36 @@ window.addEventListener("load",function() {
 	var Q = Quintus({audioSupported: [ 'mp3','ogg' ]})
         .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI, TMX, Audio")
         .setup({
-            width: 840,
-            height: 600,
+            width: 800,
+            height: 467,
     }).controls().touch().enableSound();
 
+    Q.Sprite.extend("Ball",{
+
+        init: function(p) {
+            this._super(p, {
+                
+            });
+
+            
+        },    
+
+        step: function(dt) {
+            
+        }
+
+    });
+
    	Q.scene("mainTitle",function(stage) {
-		Q.stageTMX("level1.tmx",stage);
+
+        var player = stage.insert(new Q.Ball());
+
+        Q.stageTMX("level1.tmx",stage);
+
+        stage.add("viewport");
+
+        Q.stage().viewport.scale = 0.261;
+
 	});
 
     Q.loadTMX("level1.tmx", function() {
