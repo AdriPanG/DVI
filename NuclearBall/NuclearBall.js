9544abd8f2b5ee4aa40255b936a72fd1bc7f114e
WyrnCael <juanjpri@ucm.es>
2017-05-26 16:33:13 +0200

    Lanzamiento de la pelota

--- NuclearBall/NuclearBall.js
+++ NuclearBall/NuclearBall.js
@@ -2,10 +2,10 @@ window.addEventListener("load",function() {
 
 	var Q = Quintus({audioSupported: [ 'mp3','ogg' ]})
         .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI, TMX, Audio, SVG, Physics")
-        .setup({
+        .setup('NuclearBall', {
             width: 800,
             height: 467,
-    }).controls().touch().enableSound();
+    }).enableSound();
 
     Q.Sprite.extend("Ball",{
 
@@ -13,13 +13,10 @@ window.addEventListener("load",function() {
             this._super(p, {
                 sheet: "Ball",
                 scale: 2,
-                jumpSpeed: -8000,
-                speed: 1200,
                 shape: 'circle',
-                color: 'red',
-                r: 8,
+                r: 18,
                 restitution: 0.5,
-                density: 2,
+                density: 0.5,
                 x: 280,
                 y: 1700,
                 dx: 0,
@@ -29,14 +26,12 @@ window.addEventListener("load",function() {
             });
 
             this.add('physics');
-            this.on("fire",this,"fire");
         },
 
         fire: function() {
             this.p.dx = Math.cos(this.p.angle / 180 * Math.PI),
             this.p.dy = Math.sin(this.p.angle / 180 * Math.PI),
-            console.log("x: " + this.p.dx + " y: " + this.p.dy);
-          this.physics.velocity(this.p.dx*400,this.p.dy*400);
+            this.physics.velocity(this.p.dx*4000,this.p.dy*4000);
         },
 
         step: function(dt){
@@ -113,37 +108,43 @@ window.addEventListener("load",function() {
         stage.add("world");
         Q.stageTMX("level1.tmx",stage);   
         stage.add("viewport");
-        stage.cannon = stage.insert(new Q.Cannon());
         stage.ball = stage.insert(new Q.Ball());
-
              
         Q.stage().viewport.scale = 0.261;  
+
+        Q.state.set({"lanzada" : 0})
 
 	});  
 
      var cannonMove = function(e) {
-        
+        if(Q.stage(0) && Q.state.get("lanzada") == 0){
             var ball = Q.stage(0).ball,
             touch = e.changedTouches ?  
                     e.changedTouches[0] : e,
+            canvas = document.getElementById("NuclearBall"),
+            ClientRect = canvas.getBoundingClientRect(),
             point = {
-                x: Q.touchInput.touchPos.p.x, 
-                y: Q.touchInput.touchPos.p.y 
-            };            
+                x: Math.round((e.clientX - ClientRect.left)  / 0.261),
+                y: Math.round((e.clientY - ClientRect.top) / 0.261)
+            };
        
             var angle = Math.atan2(point.y - ball.p.y,
                                point.x - ball.p.x);
-            ball.p.angle = angle * 180 / Math.PI;
-            e.preventDefault();
-        };  
+            ball.physics.angle(angle * 180 / Math.PI);
+            e.preventDefault();  
+        }
+    };  
 
-     Q._each(["touchstart","mousemove","touchmove"],function(evt) {
+    Q._each(["touchstart","mousemove","touchmove"],function(evt) {            
             Q.wrapper.addEventListener(evt,cannonMove);
         },this);
 
     var canonFire=function(e) {
-        Q.stage(0).ball.fire();
-        e.preventDefault();
+        if(Q.state.get("lanzada") == 0){
+            Q.state.set({"lanzada" : 1});
+            Q.stage(0).ball.fire();
+            e.preventDefault();
+        }
     }
 
     Q._each(["touchend","mouseup"],function(evt) {
